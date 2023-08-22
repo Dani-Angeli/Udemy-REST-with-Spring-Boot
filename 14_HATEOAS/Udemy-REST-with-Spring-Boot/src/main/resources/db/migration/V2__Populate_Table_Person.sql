1689aa8d77ca860468e4d3e16065f686436496b9
Danilo Angeli <dani.angeli97@gmail.com>
2023-08-22 13:18:24 -0300

    HATEOAS Implementation

---
+++ 14_HATEOAS/Udemy-REST-with-Spring-Boot/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`,`last_name`) VALUES
+      (1,'Curitiba','Danilo','Male','Angeli'),
+      (2,'Italy','Leonardo','Male','Da Vinci'),
+      (5,'India','Mahatma','Male','Gandhi'),
+      (6,'Kentucky - US','Muhammad','Male','Ali'),
+      (10,'Mvezo - South Africa','Nelson','Male','Mandela'),
+      (11,'Smiljan - Croatia','Nikola','Male','Tesla');
